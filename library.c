0da4c2905eeb637a1b7e8031b970a3b3c1b84681
Klaas tom Dieck <klaas.tomdieck@gmail.com>
2023-12-19 22:24:16 +0100

    Stupod error because I was using ht as a variable name?

--- library.c
+++ library.c
@@ -2,6 +2,31 @@
 
 #include <stdio.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+int main() {
+    ht* table = ht_create();
+
+    char* key = "key";
+    char* value = "value";
+
+    char* key2 = "key2";
+    char* value2 = "value2";
+
+    if(table->entries[0].key != NULL) {
+        printf("FAILED table entry key at 0 is %p instead of NULL", &table->entries[0].key);
+    }
+    else {
+        printf("worked lol");
+    }
+    ht_insert(table, key, value);
+
+    char* retrieved = ht_get(table, key);
+
+    if(!strcmp(retrieved, value)) {
+        printf("Success");
+    }
+
+    ht_insert(table, key2, value2);
+
+
+
 }
--- library.h
+++ library.h
@@ -7,12 +7,12 @@
 
 #define FNVOFFSETBASIS  0xcbf29ce484222325
 #define FNVPRIME        0x00000100000001B3
-#define STARTINGSIZE    128
+#define STARTINGSIZE    0x1
 
 
 static uint64_t hash_key(const char* buf) {
     uint64_t hash = FNVOFFSETBASIS;
-    for(const char* pos = buf; pos != NULL; ++pos) {
+    for(const char* pos = buf; *pos != '\0'; ++pos) {
         hash = hash * FNVPRIME;
         hash = hash ^ (uint64_t)pos;
     }
@@ -21,34 +21,33 @@ static uint64_t hash_key(const char* buf) {
 
 typedef struct _ht_Entry {
     const char* key;
-    void* bucket;
+    char* bucket;
 } ht_Entry;
 
 typedef struct _ht{
-    ht_Entry* entries;
+    size_t used;
     size_t capacity;
-    size_t length;
-
-    size_t index;
+    ht_Entry* entries;
 } ht;
 
 
-ht* create_ht() {
-    ht* ht = calloc(1, sizeof(ht));
-    if(ht == NULL) {
+ht* ht_create() {
+    ht* table = calloc(1, sizeof(ht));
+    if(table == NULL) {
         return NULL;
     }
-    ht->capacity = STARTINGSIZE;
-    ht->entries = calloc(STARTINGSIZE, sizeof(ht_Entry));
-    if(ht->entries == NULL) {
+    table->capacity = (size_t) STARTINGSIZE;
+    table->entries = (ht_Entry*) calloc(STARTINGSIZE, sizeof(ht_Entry));
+    if(table->entries == NULL) {
         return NULL;
     }
-    ht->length = 0;
-    return ht;
+    table->used = 0;
+    return table;
 }
 
 void free_ht(ht* table) {
     for(size_t i = 0; i < table->capacity; i++) {
+        if(table->entries[i].key == NULL)
         free(table->entries[i].bucket);
     }
     free(table->entries);
@@ -57,13 +56,36 @@ void free_ht(ht* table) {
 
 ht* ht_realloc(ht* table) {
     ht* new_table = (ht*) calloc(1, sizeof(ht));
-    new_table->length = table->length;
-    new_table->capacity = table->capacity * 2;
-    // Insert old values
-    for(size_t i = 0; i < table->length; ++i) {
-        uint64_t hash = hash_key()
+    if (new_table == NULL) {
+        return NULL;
     }
-    return NULL;
+    new_table->used = table->used;
+    new_table->capacity = table->capacity * 2;
+    new_table->entries = (ht_Entry*) calloc(table->capacity * 2, sizeof(ht_Entry));
+    if (new_table->entries == NULL) {
+        return NULL;
+    }
+
+    // Insert old values
+    for(size_t i = 0; i < table->used; ++i) {
+        ht_Entry entry = table->entries[i];
+        uint64_t hash = hash_key(entry.key) % new_table->capacity;
+        if (new_table->entries[hash].key == NULL) {
+            memcpy((void*)new_table->entries[hash].key, table->entries[i].key, strlen(table->entries[i].key));
+            memcpy((void*)new_table->entries[hash].bucket, table->entries[i].bucket, strlen(table->entries[i].bucket));
+        }
+        else {
+            hash += 1; // Might save us from one loop
+            while(new_table->entries[hash].key != NULL) {
+                hash += 1;
+            }
+            memcpy((void*)new_table->entries[hash].key, table->entries[i].key, strlen(table->entries[i].key));
+            memcpy((void*)new_table->entries[hash].bucket, table->entries[i].bucket, strlen(table->entries[i].bucket));
+        }
+        new_table->used += 1;
+    }
+    free_ht(table);
+    return new_table;
 }
 
 
@@ -71,9 +93,9 @@ ht* ht_realloc(ht* table) {
 /* returns a void* pointer to the
  *
  */
-void* ht_get(ht* table, const char* key) {
+const char* ht_get(ht* table, const char* key) {
     uint64_t hash = hash_key(key) % table->capacity;
-    if(hash >= table->length) {
+    if(hash >= table->used) {
         return NULL;
     }
     ht_Entry* entry = &table->entries[hash];
@@ -81,7 +103,11 @@ void* ht_get(ht* table, const char* key) {
         return NULL;
     }
     if((hash_key(entry->key) % table->capacity) != hash) {
-        // We iterate to the next entry to see if it matches and continue if not, till the reach the length or capacity
+        // We iterate to the next entry to see if it matches and continue if not, till the reach the used or capacity
+        while(!strcmp(table->entries[hash].key, key)) {
+            hash += 1;
+        }
+        entry = &table->entries[hash];
     }
     return entry->bucket;
 }
@@ -90,14 +116,24 @@ ht_Entry* ht_insert(ht* table, const char* key, void* object) {
     if(table == NULL) {
         return NULL;
     }
-    if(table->capacity <= table->length) {
+    if(table->capacity <= table->used) {
         table = ht_realloc(table);
         if (table == NULL) {
             return NULL;
         }
+        return ht_insert(table, key, object);
     }
-    uint64_t hash = hash_key(key) % table->capacity;
 
+    uint64_t hash = hash_key(key) % table->capacity;
+    while(table->entries[hash].key != NULL) {
+        hash += 1;
+    }
+    table->entries[hash].key = (char*) calloc(strlen(key) + 1,sizeof(char));
+    table->entries[hash].bucket = (char*) calloc(strlen(object) + 1, sizeof(char));
+    table->entries[hash].key = strcpy((char*)table->entries[hash].key, key);
+    table->entries[hash].bucket = strcpy((char*)table->entries[hash].bucket, object);
+    table->used += 1;
+    return &table->entries[hash];
 }
 
 
